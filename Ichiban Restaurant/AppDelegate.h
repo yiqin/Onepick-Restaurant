7341f8d23624fcb45027dc3335c7024ad35a0e5a
yiqin <yiqin.mems@gmail.com>
2014-06-01 08:21:32 -0700

    Initial Commit

---
+++ Ichiban Restaurant/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ichiban Restaurant
+//
+//  Created by yiqin on 6/1/14.
+//  Copyright (c) 2014 yipick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ichiban RestaurantTests/Ichiban_RestaurantTests.m
@@ -0,0 +1,34 @@
+//
+//  Ichiban_RestaurantTests.m
+//  Ichiban RestaurantTests
+//
+//  Created by yiqin on 6/1/14.
+//  Copyright (c) 2014 yipick. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ichiban_RestaurantTests : XCTestCase
+
+@end
+
+@implementation Ichiban_RestaurantTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
